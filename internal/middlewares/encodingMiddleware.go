ae387786aa1b16f0e23b14baaf298e303caebf2e
Bond <test@no-example.ru>
2025-05-11 14:04:10 +0300

    fix status created 8st increment

--- internal/middlewares/encodingMiddleware.go
+++ internal/middlewares/encodingMiddleware.go
@@ -30,7 +30,7 @@ func (c *compressWriter) Write(p []byte) (int, error) {
 }
 
 func (c *compressWriter) WriteHeader(statusCode int) {
-	if statusCode < 300 {
+	if statusCode == http.StatusCreated {
 		c.w.Header().Set("Content-Encoding", "gzip")
 	}
 	c.w.WriteHeader(statusCode)
