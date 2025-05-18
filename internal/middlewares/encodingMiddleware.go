62a3348792d2a81e910154042c4cc340c071dbdd
Bond <test@no-example.ru>
2025-05-18 22:31:21 +0300

    fix result 4 8st increment

--- internal/middlewares/encodingMiddleware.go
+++ internal/middlewares/encodingMiddleware.go
@@ -9,63 +9,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type compressWriter struct {
-	w  http.ResponseWriter
-	zw *gzip.Writer
-}
-
-func newCompressWriter(w http.ResponseWriter) *compressWriter {
-	return &compressWriter{
-		w:  w,
-		zw: gzip.NewWriter(w),
-	}
-}
-
-func (c *compressWriter) Header() http.Header {
-	return c.w.Header()
-}
-
-func (c *compressWriter) Write(p []byte) (int, error) {
-	return c.zw.Write(p)
-}
-
-func (c *compressWriter) WriteHeader(statusCode int) {
-	c.w.Header().Set("Content-Encoding", "gzip")
-	c.w.WriteHeader(statusCode)
-}
-
-func (c *compressWriter) Close() error {
-	return c.zw.Close()
-}
-
-type compressReader struct {
-	r  io.ReadCloser
-	zr *gzip.Reader
-}
-
-func newCompressReader(r io.ReadCloser) (*compressReader, error) {
-	zr, err := gzip.NewReader(r)
-	if err != nil {
-		return nil, err
-	}
-
-	return &compressReader{
-		r:  r,
-		zr: zr,
-	}, nil
-}
-
-func (c compressReader) Read(p []byte) (n int, err error) {
-	return c.zr.Read(p)
-}
-
-func (c *compressReader) Close() error {
-	if err := c.r.Close(); err != nil {
-		return err
-	}
-	return c.zr.Close()
-}
-
 type ginResponseWriter struct {
 	gin.ResponseWriter
 	writer io.Writer
@@ -80,28 +23,32 @@ func GinGzipMiddleware() gin.HandlerFunc {
 		r := c.Request
 		w := c.Writer
 
+		// Декодируем входящий gzip-запрос
 		if strings.Contains(r.Header.Get("Content-Encoding"), "gzip") &&
 			strings.HasPrefix(r.Header.Get("Content-Type"), "application/json") &&
 			r.Method == http.MethodPost {
 
-			cr, err := newCompressReader(r.Body)
+			gr, err := gzip.NewReader(r.Body)
 			if err != nil {
 				c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": "failed to decompress request"})
 				return
 			}
-			defer cr.Close()
-			r.Body = cr
+			defer gr.Close()
+			r.Body = io.NopCloser(gr)
 		}
 
-		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") && r.Method == http.MethodPost {
-			cw := newCompressWriter(w)
-			defer cw.Close()
+		// Сжимаем исходящий ответ, если клиент поддерживает gzip
+		if strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") &&
+			strings.HasPrefix(r.Header.Get("Content-Type"), "application/json") {
 
+			gzw := gzip.NewWriter(w)
+			defer gzw.Close()
+
+			c.Header("Content-Encoding", "gzip")
 			c.Writer = &ginResponseWriter{
 				ResponseWriter: w,
-				writer:         cw.zw,
+				writer:         gzw,
 			}
-			c.Header("Content-Encoding", "gzip")
 		}
 
 		c.Next()
