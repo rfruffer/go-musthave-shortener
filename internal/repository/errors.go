cfaac867d8c4d26a4b7d20912f3afc8d08e9de4b
Bond <test@no-example.ru>
2025-08-14 19:27:20 +0300

    18st increment 3st

--- internal/repository/errors.go
+++ internal/repository/errors.go
@@ -3,8 +3,9 @@ package repository
 import "errors"
 
 var (
-	//ErrAlreadyExists Ошибка что URL уже существует
+	// ErrAlreadyExists — ошибка, означающая, что короткий URL уже существует.
 	ErrAlreadyExists = errors.New("short url already exists")
-	//ErrGone Ошибка что URL уже удален
+
+	// ErrGone — ошибка, означающая, что URL был удалён.
 	ErrGone = errors.New("URL is deleted")
 )
